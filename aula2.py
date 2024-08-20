b27113ef1e1acb5cc08f7c9a2cfc79bd1fe6e2ab
Fabricio Abdalla <fabricioabdcosta@gmail.com>
2024-08-20 10:04:21 -0400

    Aulas Iniciais Python-Udemy

---
+++ aula2.py
@@ -0,0 +1,5 @@
+# \r\n return e line feed -> CRLF
+# \n -> LF
+print(12, 34, sep="-", end='\r\n')
+print(56, 78, sep='_', end='\r\n')
+print(9, 10, sep='_', end='##')
---
+++ aula3.py
@@ -0,0 +1,19 @@
+"""
+DocString
+
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Apas simples
+print('Teste')
+print('Teste  "de escape"')
+# Aspas duplas
+print("Teste")
+print(2, "Teste  'de escape'")
+# Escape
+print("Teste \" de escape\"")
+# r (exibir caractere de escape)
+print(r"Teste \" de escape\"")
