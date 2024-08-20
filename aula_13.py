e9a730aaf7a903783cadf5f19a1c3021164960dc
Fabricio Abdalla <fabricioabdcosta@gmail.com>
2024-08-20 13:01:00 -0400

    Introdução a Fstring

---
+++ aula_13.py
@@ -0,0 +1,8 @@
+nome = "Fabrício Abdalla"
+altura = 1.75
+peso = 90
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem {altura:,.2f} de altura pesa {peso} quilos e seu IMC é {imc:.2f}'
+
+print(linha_1)
