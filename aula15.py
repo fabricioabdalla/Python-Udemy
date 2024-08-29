0360f4de2f6cf07c4764eee0b20f6dc40c2670ab
Abdalla <fabricioabdcosta@gmail.com>
2024-08-29 15:58:38 -0400

    Input e conversão de valores

---
+++ aula15.py
@@ -0,0 +1,9 @@
+nome = input('Informe o seu nome: ')
+n1 = input('Digite um número: ')
+n2 = int(input('Informe outro número: '))
+
+#é possivel fazer a coersão dos números antes ou depois ex:
+n1_int= int(n1)
+
+print(f'A sona dos números informados é: {n1 + n2}')
+print(f'Seu nome é {nome} {nome=}')
